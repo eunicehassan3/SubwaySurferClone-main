a6465d6bfd63adcde7bd0978cce798f2059a04ea
eunicehassan3 <euniceh.info@gmail.com>
2025-03-17 18:12:26 -0500

    add a script to try and get the game to get faster as time goes on

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 public class BackgroundBehavior : MonoBehaviour
 {
     public float speed;
+    public float acceleration; 
+    private float currentSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         speed = 2;
+        currentSpeed = speed;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-transform.forward * speed * Time.deltaTime);
+        currentSpeed += acceleration * Time.deltaTime;
+        transform.Translate(-transform.forward * currentSpeed * Time.deltaTime);
+        Debug.Log("Current Speed" +  currentSpeed);
     }
+
+
 }
